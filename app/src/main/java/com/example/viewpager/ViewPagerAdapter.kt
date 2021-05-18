2a3cdddefc62cfd0d89cf1d4b271d48fdba2c685
wjdwns <wjdwns3744@naver.com>
2021-05-18 20:51:14 +0900

    Initial commit

---
+++ app/src/main/java/com/example/viewpager/ViewPagerAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.viewpager
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity){
+
+    var fragments : ArrayList<Fragment> = ArrayList()
+}
